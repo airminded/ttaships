e64347d2a76b2ccd6282c024b5b6b8f23b5bb9f1
Brett Holman <airminded@users.noreply.github.com>
2023-11-25 17:27:53 +1100

    Update ttaships.py

--- ttaships.py
+++ ttaships.py
@@ -11,72 +11,6 @@ from urllib.parse import urlparse
 #import helpers
 #import configLog
 
-def login_to_bluesky(BLUESKY_EMAIL, BLUESKY_PASSWORD):
-    print(f"BLUESKY_EMAIL: {BLUESKY_EMAIL}")
-    #logger, _ = configLog.configure_logging()
-    try:
-        client = Client()
-        client.login(BLUESKY_EMAIL, BLUESKY_PASSWORD)
-        print(f"1: Successful log in to Bluesky.")
-        #logger.debug("Successfully logged in to Bluesky.")
-    except Exception as e:
-        print(f"1: Failed to log in to Bluesky: {e}")
-
-def post_to_bluesky(BLUESKY_EMAIL, BLUESKY_PASSWORD, text, image_locations, alt_texts):
-    #logger, _ = configLog.configure_logging()
-
-    try:
-        login_to_bluesky(BLUESKY_EMAIL, BLUESKY_PASSWORD)
-    except Exception as e:
-        print(f"2: Failed to log in to Bluesky: {e}")
-        return False
-
-    #text = helpers.strip_html_tags(text)
-    #logger.debug(f"Stripped text: {text}")
-
-    images = []
-    for idx, image_location in enumerate(image_locations):
-        try:
-            # Parse the URL and get the path
-            url_parts = urlparse(image_location)
-            local_file_path = url_parts.path[1:]  # Remove the leading '/'
-
-            # Debug: log the current file path
-            #logger.debug(f"Processing image file: {local_file_path}")
-
-            # Open the image file from its location
-            with open(local_file_path, 'rb') as img_file:
-                img_data = img_file.read()
-
-            upload = client.com.atproto.repo.upload_blob(img_data)
-            images.append(models.AppBskyEmbedImages.Image(alt=alt_texts[idx], image=upload.blob))
-            #logger.debug(f"Uploaded image: {upload.blob}")
-        except Exception as e:
-            # Exception handling: log the error and local file path
-            #logger.exception(f"Unable to process the image file at {local_file_path} for Bluesky. Error: {e}")
-            return False
-
-    embed = models.AppBskyEmbedImages.Main(images=images) if images else None
-    #facets = helpers.generate_facets_from_links_in_text(text) if helpers.URL_PATTERN.search(text) else None
-    #logger.debug(f"Embed: {embed}, Facets: {facets}")
-
-    try:
-        client.com.atproto.repo.create_record(
-            models.ComAtprotoRepoCreateRecord.Data(
-                repo=client.me.did,
-                collection='app.bsky.feed.post',
-                record=models.AppBskyFeedPost.Main(
-                    createdAt=datetime.now().isoformat(), text=text, embed=embed, facets=facets
-                ),
-            )
-        )
-        #logger.debug("Bluesky post created.")
-    except Exception as e:
-        #logger.exception(f"Failed to create Bluesky post: {e}")
-        return False
-
-    return True
-
 def main():
     from os import environ
 
@@ -103,6 +37,11 @@ def main():
         access_token=MASTODON_ACCESS_TOKEN,
         api_base_url=MASTODON_BASE_URL
     )
+
+    # Bluesky authentication
+    client = Client()
+    client.login(BLUESKY_EMAIL, BLUESKY_PASSWORD)
+
 
     # Get image from Cloudinary
     out = cloudinary.api.resources(type="upload", max_results=500)
@@ -142,9 +81,10 @@ def main():
     mastodon.media_post(image)
     mastodon.status_post(post, media_ids=[mastodon.media_post(image)['id']])
 
-    # Post to Bluesky with text, image locations, and alt texts
-    if not post_to_bluesky(BLUESKY_EMAIL, BLUESKY_PASSWORD, post, [image], [shipname]):
-        print("3: Failed to post to Bluesky")
+    # Post to Bluesky with image
+    client.send_image(
+            text='post, image=image, image_alt=''
+        )
 
     # Delete image from Cloudinary
     cloudinary.uploader.destroy(name)
