bd30ca981cae4731ad1aa576d04423e986d6364a
Brett Holman <airminded@users.noreply.github.com>
2024-03-17 18:13:05 +1100

    Update ttaships.py

--- ttaships.py
+++ ttaships.py
@@ -84,14 +84,14 @@ def main():
     #processed_image = Image.open(img_byte_array)
     image_data = img_byte_array.getvalue()
 
-    hashtag = 'TerranTradeAuthority'
+    hashtag = 'AIArt'
     
     # Create Tag facet object
     tag_facet = Tag(tag=hashtag)  # Assuming the hashtag should be used as the tag value
     
     # Post to Bluesky with image
     client.send_image(
-            text=post, image=image_data, image_alt='', facet=[tag_facet]
+            text=post, image=image_data, image_alt='', facets=[tag_facet]
         )
 
     # Delete image from Cloudinary
